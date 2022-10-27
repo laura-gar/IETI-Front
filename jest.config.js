63efdcc9c99660b55a6cf66b7474766e4da90319
Laura Garcia <laura.garcia-l@outlook.com>
2022-10-27 08:14:28 -0500

    Test created

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+    moduleNameMapper: {
+    "\\.(css)$":  "<rootDir>/src/__mocks__/styleMock.js",
+    },
+  };
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { shallow } from  "enzyme";
+import { renderHook } from '@testing-library/react-hooks';
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import  App  from  "./App";
+import Home from "./views/Home/Home";
+import Login from "./views/Login/Login";
+import useLogin from "./views/Login/hooks/useLogin";
+import * as React from "react";
+import * as Router from "react-router-dom"
+import { ThemeContext } from "../src/ThemeContext";
+import useHome from "./views/Home/hooks/useHome";
+
+
+describe("<App/>", () => {
+	it("should render correctly", () => {
+		const  wrapper = shallow(<App  />);
+		expect(wrapper).toHaveLength(1);
+	});
+});
+
+describe("Test useLogin hook", () => {
+	it("should render Login hook correctly", () => {
+		const { result } = renderHook(() => useLogin());
+		
+	});
+});
+
+describe("Test useHome hook", () => {
+	it("should render Home hook correctly", () => {
+		const { result } = renderHook(() => useHome());
+		
+	});
 });
